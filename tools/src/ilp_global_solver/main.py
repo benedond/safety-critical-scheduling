c928f540c7da57c3584dc9f09801877c450b26b8
benedond <benedond@fel.cvut.cz>
2021-06-29 15:27:58 +0200

    Write main for ilp_global.

---
+++ tools/src/ilp_global_solver/main.py
@@ -0,0 +1,42 @@
+import os 
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from common import instance
+from common import arg_parser
+import ilp_global_solver
+import json
+
+
+if __name__ == "__main__":
+
+    ap = arg_parser.ArgParser()
+    input_filename = ap.get_arg_value("--input")
+
+    if input_filename:
+        json_data = instance.read_json_from_file(input_filename)
+    else:
+        json_data = json.load(sys.stdin)
+
+    # parse the instance
+    try:
+        env = instance.parse_environment(json_data)
+        acs = instance.parse_assignment_characteristics(json_data)
+    except:
+        print("failed to parse input json", file=sys.stderr)
+
+    # solve the instance
+    try:
+        solver = ilp_global_solver.Solver(ap, env, acs)
+        solution, tasks = solver.solve()
+
+        instance.write_solution(json_data, solution)
+        instance.write_tasks(json_data, tasks)
+    except:
+        print("failed to solve the instance", file=sys.stderr)
+
+    output_filename = ap.get_arg_value("--output")
+    if output_filename:
+        instance.write_to_file(json_data, output_filename)
+    else:
+        json.dump(json_data, sys.stdout, indent=4)
