f9900732e18d9d2f65e348c4097b4adf118342ab
benedond <benedond@fel.cvut.cz>
2021-07-14 15:58:03 +0200

    Start implementing B&B.

--- tools/src/decomposed_solver/decomposed_solver.py
+++ tools/src/decomposed_solver/decomposed_solver.py
@@ -5,44 +5,20 @@ from typing import List, Tuple, Mapping
 import gurobipy as grb
 import sys
 import time
+import itertools
 
 
-class MasterModel:
-    def __init__(self, patterns: List[instance.Pattern], major_frame_length: int, tasks: List[str]):
+class ILPSolver:    
+    def __init__(self):
         self.model = None
-        self.alpha = None
-        self.patterns = patterns
-        self.major_frame_length = major_frame_length
-        self.tasks = tasks
-        self.c0 = None
-        self.ct = {}
         self.solved = False
         self.feasible = False
-        
-        self._init_model()
+        self.solving_time = None        
         
     def _init_model(self):
-        m = grb.Model("Master Model")
-        
-        # variables
-        alpha = m.addVars(len(self.patterns), vtype=grb.GRB.CONTINUOUS, lb=0, name="alpha")
-        
-        # constraints
-        # - major frame length        
-        self.c0 = m.addConstr(grb.quicksum(alpha[i] * p.length for i, p in enumerate(self.patterns)) <= self.major_frame_length, name="C:major_frame")
-        
-        # - patterns cover tasks
-        for t in self.tasks:                        
-            self.ct[t] = m.addConstr(grb.quicksum(alpha[i] for i, p in enumerate(self.patterns) if t in p.task_mapping) >= 1, 
-                                                  name="C:task_cover_{}".format(t))
-        
-        # objective
-        m.setObjective(grb.quicksum(alpha[i] * p.cost for i, p in enumerate(self.patterns)), sense=grb.GRB.MINIMIZE)
-        
-        self.model = m
-        self.alpha = alpha
-        
-    def solve(self):
+        raise NotImplementedError
+    
+    def solve(self):            
         t_start=time.time()
         self.model.optimize()
         t_end=time.time()
@@ -51,16 +27,270 @@ class MasterModel:
             self.model.Status == grb.GRB.TIME_LIMIT and self.model.SolCount > 0) else False
         
         self.solved = True
-        self.feasible = s_feasible        
+        self.feasible = s_feasible   
+        self.solving_time = t_end - t_start
+
+
+class MasterModel(ILPSolver):
+    def __init__(self, patterns: List[instance.Pattern], major_frame_length: int, tasks: List[str]):        
+        self.alpha = None
+        self.patterns = patterns
+        self.major_frame_length = major_frame_length
+        self.tasks = tasks
+        self.c0 = None
+        self.ct = {}        
         
+        self._init_model()
+        
+    def _init_model(self):
+        m = grb.Model("Master Model")
+        
+        # variables
+        alpha = m.addVars(len(self.patterns), vtype=grb.GRB.CONTINUOUS, lb=0, ub=1, name="alpha")
+        
+        # constraints
+        # - major frame length        
+        self.c0 = m.addConstr(grb.quicksum(alpha[i] * p.length for i, p in enumerate(self.patterns)) <= self.major_frame_length, name="C:major_frame")
+        
+        # - patterns cover tasks
+        for t in self.tasks:                        
+            self.ct[t] = m.addConstr(grb.quicksum(alpha[i] for i, p in enumerate(self.patterns) if t in p.task_mapping) >= 1, 
+                                                  name="C:task_cover_{}".format(t))  # TODO: should be >= 1 or == 1 ? 
+        
+        # objective
+        m.setObjective(grb.quicksum(alpha[i] * p.cost for i, p in enumerate(self.patterns)), sense=grb.GRB.MINIMIZE)
+        
+        self.model = m
+        self.alpha = alpha
+        
+    def is_solution_integer(self) -> bool:
+        EPS = 1e-4
+        for a in self.alpha:
+            if EPS < a.X < 1 - EPS:
+                return False
+        return True        
+
+                     
     def get_dual_prices(self) -> Tuple[float, Mapping[str, float]]:
         if not self.solved:
             print("warning: get_dual_prices cannot be called for unsolved model; call solve() first", file=sys.stderr)
         if not self.feasible:
             print("warning: model is not feasible, dual prices cannot be obtained", file=sys.stderr)
         return self.c0.Pi, {t: self.ct[t].Pi for t in self.tasks}
+    
+    def get_solution(self):
+        sol = []
+        for i, p in enumerate(self.patterns):
+            if self.alpha[i].X > 0:  # TODO: comparison
+                sol.append({"index": i, "value": self.alpha[i].X})
+                
+        return sol
+    
+    def get_selected_patterns(self) -> List[instance.Pattern]:        
+        return [self.patterns[i] for i,a in enumerate(self.alpha) if a.X > 1e-4]
+    
+    def get_solution_and_tasks(self,
+                               env: instance.Environment,
+                               acs:  List[instance.AssignmentCharacteristic],
+                               task_map: Mapping[str, instance.Task]) -> Tuple[instance.Solution, List[instance.Task]]:
+        task_to_ra = instance.get_task_to_ra(task_map, acs)
+        solution=None
+        tasks=[]
+
+        s_feasible=True if self.model.Status == grb.GRB.OPTIMAL or (
+            model.Status == grb.GRB.TIME_LIMIT and model.SolCount > 0) else False
+        s_solver_name="BAP-MasterModel"
+        s_solution_time=int(round(self.solving_time*1000))  # to ms
+        s_windows=[]
+        s_metadata={"objective": -1}
+
+        if s_feasible:
+            s_metadata["objective"] = str(self.model.ObjVal)
+            
+            for j, p in enumerate(self.patterns):
+                if self.alpha[j].X > 0:  # TODO: comparison
+                    window_length = p.length
+                    window_tasks_assignments=[]
+                    
+                if window_length < 1:
+                    continue
+                
+                # processor-unit allocation (start with all processors empty)
+                pu_allocations={p.name: 0 for p in env.processors_list}
+                                
+                for t in p.task_mapping:
+                    task_processors = []
+                    for p in task_to_ra[t].processors:  
+                        window_tasks_assignments.append(instance.TaskAssignment(task=t,
+                                                                                processor=p.processor,
+                                                                                processing_unit=pu_allocations[p.processor],
+                                                                                start=0,
+                                                                                length=task_to_ra[t].length))
+                        
+                                                      
+                        # increment the processor units
+                        pu_allocations[p.processor] += p.processing_units                                
+                        task_processors.append(instance.ProcessorAssignment(p.processor, p.processing_units))                                 
+                                
+                    tasks.append(instance.Task(name=t,
+                                               command=task_map[t].command,                                                
+                                               length=task_to_ra[t].length,
+                                               assignment_index=p.task_mapping[t],
+                                               processors=task_processors))                    
+            
+                s_windows.append(instance.Window(window_length, window_tasks_assignments))
+        else:  # infeasible solution            
+            s_feasible=False
+            if self.arg_parser.is_arg_present("--iis-output"):
+                model.computeIIS()
+                model.write(self.arg_parser.get_arg_value("--iis-output") + ".ilp")        
+        solution = instance.Solution(s_feasible, s_solver_name, s_solution_time, s_metadata, s_windows)
         
+        return (solution, tasks)
+
         
+
+
+def get_pair(selected_patterns: List[instance.Pattern], on_same: List[Tuple[str,str]], on_diff: List[Tuple[str,str]]) -> Tuple[str,str]:
+    def pair_selection_heuristis(lst: List[Tuple[str,str]]) -> Tuple[str,str]:
+        return lst[0]  # TODO: implement some heuristic
+    
+    all_tasks = set()
+    for p in selected_patterns:
+        all_tasks.update(p.task_mapping.keys())
+    
+    all_pairs = itertools.combinations(all_tasks, 2)  # combine the tasks
+    all_pairs = [p for p in all_pairs if p not in on_same and p not in on_diff]  # filter already used tasks
+    
+    if all_pairs:
+        return pair_selection_heuristis(all_pairs)
+    else:
+        return None
+
+ 
+class SubproblemModelILP(ILPSolver):
+    
+    def __init__(self, env: instance.Environment, acs: List[instance.AssignmentCharacteristic], dual_prices: Tuple[float, Mapping[str, float]]):
+        self.env = env
+        self.acs = acs
+        self.dual_prices = dual_prices 
+        self.x_ik = None       
+        self.l = None
+        
+        self._init_model()        
+        
+    def _init_model(self):
+        m = grb.Model("Subproblem-ILP")
+        num_tasks = len(self.acs)
+
+        # variables
+        x_ik = m.addVars([(i, k)
+                          for i in range(num_tasks)                               
+                          for k in range(len(self.acs[i].resource_assignmnets))],
+                          vtype=grb.GRB.BINARY,
+                          name="x")        
+        l = m.addVar(vtype=grb.GRB.INTEGER, lb=0, name="l")
+        B = m.addVar(vtype=grb.GRB.CONTINUOUS, lb=0, name="B")                    
+        
+        # constraints
+        # - B value
+        M = self.env.major_frame_length * max([self.acs[i].resource_assignmnets[k].intercept
+                                                for i in range(num_tasks)
+                                                for k in range(len(self.acs[i].resource_assignmnets))])
+        m.addConstrs(B >= l * self.acs[i].resource_assignmnets[k].intercept - M * (1-x_ik[i,k])
+                     for i in range(num_tasks)
+                     for k in range(len(self.acs[i].resource_assignmnets)))
+        
+        # - assign task to at most one resource
+        m.addConstrs(x_ik.sum(i, "*") <= 1
+                    for i in range(num_tasks))
+        
+        # - respect capacity of the resource        
+        m.addConstrs((grb.quicksum(x_ik[i, k] * acp.processing_units
+                                       for i in range(num_tasks)
+                                       for k in range(len(self.acs[i].resource_assignmnets))
+                                       for acp in self.acs[i].resource_assignmnets[k].processors if acp.processor == processor.name)
+                          <= processor.processing_units
+                          for processor in self.env.processors_list),
+                         name="resource capacity")
+        
+        # - constrain window length
+        m.addConstrs(l >= x_ik[i, k] * self.acs[i].resource_assignmnets[k].length
+                     for i in range(num_tasks)
+                     for k in range(len(self.acs[i].resource_assignmnets)))
+        
+        # objective
+        obj = grb.quicksum(x_ik[i, k] * (self.acs[i].resource_assignmnets[k].length
+                                         * self.acs[i].resource_assignmnets[k].slope 
+                                         -  self.dual_prices[1][self.acs[i].task])
+                           for i in range(num_tasks)
+                           for k in range(len(self.acs[i].resource_assignmnets))) - l*self.dual_prices[0] + B      
+        m.setObjective(obj, sense=grb.GRB.MINIMIZE)
+        
+        self.model = m
+        self.x_ik = x_ik
+        self.l = l
+        
+    def get_pattern(self) -> instance.Pattern:
+        if not self.solved:
+            print("warning: the model was not solved", f=sys.stderr)
+            return None
+        if not self.feasible:
+            print("warning: the model is not feasible", f=sys.stderr)
+            return None
+        
+        p_len = int(round(self.l.X))
+        #p_cost = self.model.ObjVal + self.dual_prices[0] * p_len        
+        p_task_mapping = {}
+        
+        for i in range(len(self.acs)):
+            for k in range(len(self.acs[i].resource_assignmnets)):
+                if self.x_ik[i,k].X > 0.5:
+                    p_task_mapping[self.acs[i].task] = k
+                    #p_cost += self.dual_prices[1][self.acs[i].task]
+                    break
+
+        # recompute the cost manually to avoid potential numerical problems
+        pattern = instance.Pattern(0, p_len, p_task_mapping)                
+        cost = pattern.compute_cost(self.acs)
+        pattern.cost = cost
+        
+        return pattern
+   
+    def get_patterns(self) -> List[instance.Pattern]:
+        if not self.solved:
+            print("warning: the model was not solved", f=sys.stderr)
+            return None
+        if not self.feasible:
+            print("warning: the model is not feasible", f=sys.stderr)
+            return None
+        
+        patterns = []
+        
+        for sol_idx in range(self.model.SolCount):
+            self.model.setParam("SolutionNumber", sol_idx)
+            
+            if self.model.PoolObjVal < -0.0001:  # TODO: some robust check
+        
+                p_len = int(round(self.l.Xn))            
+                p_task_mapping = {}
+            
+                for i in range(len(self.acs)):
+                    for k in range(len(self.acs[i].resource_assignmnets)):
+                        if self.x_ik[i,k].Xn > 0.5:
+                            p_task_mapping[self.acs[i].task] = k                        
+                            break
+
+                # recompute the cost manually to avoid potential numerical problems
+                pattern = instance.Pattern(0, p_len, p_task_mapping)                
+                cost = pattern.compute_cost(self.acs)
+                pattern.cost = cost
+
+                patterns.append(pattern)
+                        
+        return patterns    
+        
+       
 class Solver:
 
     def __init__(self, arg_parser: ap.ArgParser, env: instance.Environment, acs: List[instance.AssignmentCharacteristic]):
@@ -70,10 +300,9 @@ class Solver:
         self.patterns = None
 
     def solve(self) -> Tuple[instance.Solution, List[instance.Task]]:
-        
-        
-        
-        
+
+        # TODO
+
         solution = None 
         tasks = None       
         return (solution, tasks)
--- tools/src/decomposed_solver/main.py
+++ tools/src/decomposed_solver/main.py
@@ -7,6 +7,134 @@ from common import instance
 from common import arg_parser
 import decomposed_solver
 import json
+from typing import List, Tuple, Mapping
+
+# python tools/src/decomposed_solver/main.py --input ./experiments/test_decomposition/IN_20.json 
+
+
+
+def branch_and_price(env: instance.Environment,
+                     acs: List[instance.AssignmentCharacteristic],
+                     on_same: List[Tuple[str,str]],
+                     on_diff: List[Tuple[str,str]],
+                     patterns: List[instance.Pattern]):
+            
+    print("info: branching, on same = {:s}, on diff = {:s}".format(str(on_same), str(on_diff)))    
+
+    # Iterate - master -> subproblem
+    while True:
+            
+        # - solve restricted master problem
+        mm = decomposed_solver.MasterModel(patterns, env.major_frame_length, [ac.task for ac in acs])
+        mm.solve()
+        
+        if not mm.feasible:
+            print("warning: master model is not feasible.", file=sys.stderr)
+            return None
+        
+        pi0, pit = mm.get_dual_prices()
+        
+        # print(pi0)
+        # for k in pit:
+        #     print("{:22s} -> {:f}".format(k,pit[k]))
+            
+        # - solve subproblem
+        ss = decomposed_solver.SubproblemModelILP(env, acs, (pi0, pit))
+        ss.solve()
+        
+        if not ss.feasible:
+            print("warning: subproblem model is not feasible.", file=sys.stderr)
+            return None
+        
+        if ss.model.ObjVal >= 0:  # no more improving patterns exist
+            break
+            # for s in mm.get_solution():
+            #     print(s)       
+            # mm.model.write("MP.sol")     
+            # mm.model.write("MP.lp") 
+            
+            # for i, p in enumerate(patterns):
+            #     print(i, p.to_dict())  
+            #     # p.check_cost(acs)      
+            #     # p.check_length(acs)                                      
+        else:
+            p = ss.get_pattern()
+            print("info: found pattern", p.to_dict())
+            patterns.append(p)                                              
+    # END of pattern generation phase ----------------------------------------------------------------------------------
+
+    # Solve master model to get the optimal solution of the relaxed problem
+    # - solve restricted master problem
+    mm = decomposed_solver.MasterModel(patterns, env.major_frame_length, [ac.task for ac in acs])
+    mm.solve()
+    
+
+    if not mm.solved:
+        print("warning: master model was not solved.", file=sys.stderr)
+        return None
+
+    # BRANCHING
+    if not mm.feasible:
+        print("warning: master model was not feasible.", file=sys.stderr)            
+        return None
+
+    # if master_model.alpha_is_not_zero():  # If alpha > 0 - there is no solution to relaxed master model without alpha
+    #     if params.PRINT_BB:
+    #         print("[B&P:] Master model - alpha is not zero: alpha =", master_model.alpha.X)
+    #     return None
+
+    if mm.model.objVal > glob_best_obj:  # Check if master model is worse than best so far solution
+        print("info: master model solution is worst than best-so-far solution.")            
+        return None
+
+    if mm.is_solution_integer():  # Check if solution is integer
+        master_solution = get_solution(master_model, p_wrap)
+        
+        # TODO: update best so far
+        # if master_model.model.objVal < glob_best_obj:
+        #     glob_best_obj = master_model.model.objVal
+        
+        # TODO: get master solution        
+        return mm.get_solution
+    else:
+        print("info: master model solution is not integer.")
+
+        # Create two branches and return better solution
+        pair = decomposed_solver.get_pair(mm.get_selected_patterns(), on_same, on_diff)        
+        
+        if pair is None:  # There was no pair to generate
+            return None
+
+        # Generate new lists
+        on_same_new = on_same.copy()
+        on_diff_new = on_diff.copy()
+        on_diff_new.append(pair)
+        on_same_new.append(pair)
+
+        p_wrap_same, p_wrap_diff = generate_new_patterns(p_wrap, pair)
+        
+        p_same = [p for p in patterns if ((pair[0] in p.task_mapping and p[1] in p.task_mapping)
+                                          or (pair[0] not in p.task_mapping and p[1] not in p.task_mapping))]
+        p_diff = [p for p in patterns if not (pair[0] in p.task_mapping and pair[1] in p.task_mapping)]
+
+        # - two branches
+        sol_same = branch_and_price(instance, on_same_new, on_diff, p_same)
+        sol_diff = branch_and_price(instance, on_same, on_diff_new, p_diff)
+
+        return get_better_sol(sol_same, sol_diff)
+
+def get_better_sol(sol1: Tuple[instance.Solution, List[instance.Task]], sol2: Tuple[instance.Solution, List[instance.Task]]) -> Tuple[instance.Solution, List[instance.Task]]:    
+    if sol1 is None and sol2 is None:
+        return None
+    elif sol1 is None and sol2 is not None:
+        return sol2
+    elif sol1 is not None and sol2 is None:
+        return sol1
+    else:
+        if sol1[0].solver_metadata["objective"] < sol2[0].solver_metadata["objective"]:
+            return sol1
+        else:
+            return sol2
 
 
 if __name__ == "__main__":
@@ -25,23 +153,7 @@ if __name__ == "__main__":
     init_data = instance.read_json_from_file("/home/benedond/Doktorat/Vyzkum/Projects/safety-critical-scheduling/experiments/test_decomposition/IN_20_solution.json")
     patterns = instance.get_patterns(init_data)
     for p in patterns:
-        print(p.to_dict())
-        
-    mm = decomposed_solver.MasterModel(patterns, env.major_frame_length, [ac.task for ac in acs])
-    mm.solve()
-    pi0, pit = mm.get_dual_prices()
+        print(p.to_dict())        
     
-    print(mm.)
-
-    # # solve    
-    # solver = decomposed_solver(ap, env, acs)
-    # solution, tasks = solver.solve()        
-
-    # instance.write_solution(json_data, solution)
-    # instance.write_tasks(json_data, tasks)    
-
-    # output_filename = ap.get_arg_value("--output")
-    # if output_filename:
-    #     instance.write_to_file(json_data, output_filename)
-    # else:
-    #     json.dump(json_data, sys.stdout, indent=4)
+    sol = branch_and_price(env, acs, [], [], patterns)
+    
