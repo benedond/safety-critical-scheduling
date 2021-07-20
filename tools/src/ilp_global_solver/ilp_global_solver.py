c2c427896cf2809b09981c1f29d6d71c030593ab
benedond <benedond@fel.cvut.cz>
2021-07-20 16:24:57 +0200

    Metadata should be string

--- tools/src/ilp_global_solver/ilp_global_solver.py
+++ tools/src/ilp_global_solver/ilp_global_solver.py
@@ -116,7 +116,7 @@ class Solver:
         s_solver_name="ILP Solver (global):predictor (Python)"
         s_solution_time=int(round((t_end - t_start)*1000))  # to ms
         s_windows=[]
-        s_metadata={"objective": -1}
+        s_metadata={"objective": "-1"}
 
         if s_feasible:
             s_metadata["objective"] = str(model.ObjVal)
