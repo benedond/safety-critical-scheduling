fd67715112f75f6d4e3c0b42898eabba05b3daa2
benedond <benedond@fel.cvut.cz>
2021-08-26 09:52:21 +0200

    Update some results.

--- experiments/16_bap_vs_global_real_instances/instances/RUN.sh
+++ experiments/16_bap_vs_global_real_instances/instances/RUN.sh
@@ -6,9 +6,9 @@ do
         ../../../tools/src/ilp_global_solver/main.py --input $i --output $i-global.out --timelimit 60 2> $i-global.err 1> $i-global.log
         ../../../tools/bin/visualizer.exe --input $i-global.out --output $i-global.png
     fi 
-    if [ ! -f $i-bap.out ]; then
-        ../../../tools/src/decomposed_solver/main.py --input $i --output $i-bap.out --log --timelimit 60 2> $i-bap.err 1> $i-bap.log
-        ../../../tools/bin/visualizer.exe --input $i-bap.out --output $i-bap.png
+    if [ ! -f $i-bap-pair.out ]; then
+        ../../../tools/src/decomposed_solver/main.py --input $i --output $i-bap-pair.out --log --timelimit 60 2> $i-bap-pair.err 1> $i-bap-pair.log
+        ../../../tools/bin/visualizer.exe --input $i-bap-pair.out --output $i-bap-pair.png
     fi 
 
     if [ ! -f $i-bap-task.out ]; then
