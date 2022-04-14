b6ecdc2e1a247fb647fb0892aa0017954669f3b1
Michal Sojka <michal.sojka@cvut.cz>
2022-04-14 13:36:04 +0200

    Unify run_imx8a and run_imx8b scripts

--- experiments/run_imx8.sh
+++ experiments/run_imx8.sh
@@ -7,7 +7,8 @@ run () {
     OUTPATH=$2
     PLAT=$3
 	TIM=$4
-        
+    FAN="${5:+--fan-cmd=fan --fan-on=0}" # if $5 has some value, replace it with --fan...
+    
     mkdir -p $OUTPATH
 	
 	for benchmark in $INPATH/*; do
@@ -18,17 +19,12 @@ run () {
 				echo "Output file already exists."
 		else	
 			cd /root/thermobench/build/benchmarks/autobench_2.0/single/
-			thermobench --verbose --time=$TIM --wait=50 --wait-timeout=60 --sensors_file=/root/thermobench/src/sensors.$PLAT --cpu-usage --column=CPU{0,1,2,3,4,5}_work_done --output=$OUTPATH/$CURBENCH.csv -- demos-sched -p per_process -c $INPATH/$CURBENCH
+			thermobench --verbose $FAN --time=$TIM --wait=50 --wait-timeout=60 --sensors_file=/root/thermobench/src/sensors.$PLAT --cpu-usage --column=CPU{0,1,2,3,4,5}_work_done --output=$OUTPATH/$CURBENCH.csv -- demos-sched -p per_process -c $INPATH/$CURBENCH
 		fi
 	done	
 }
 # ===================================================================================================
 
-INPUT_PATH=$1
-OUTPUT_PATH=$2
-PLATFORM=$3
-TIME=$4
-
 ulimit -n 10000
 
-run "$INPUT_PATH" "$OUTPUT_PATH" "$PLATFORM" "$TIME"
+run "$@"
