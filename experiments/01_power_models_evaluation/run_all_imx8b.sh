633e302d5cd3b8adb9b6a6d40f0665da5b964b23
benedond <benedond@fel.cvut.cz>
2022-04-14 13:57:56 +0200

    Add fan options.

--- experiments/01_power_models_evaluation/run_all_imx8b.sh
+++ experiments/01_power_models_evaluation/run_all_imx8b.sh
@@ -1,8 +1,8 @@
 /root/JSA/power_experiment/run_imx8b.sh /root/JSA/power_experiment/demos_configurations/scale-1x/imx8b/all/ /root/JSA/power_experiment/measurements/scale-1x/imx8b/all/ imx8 10
 /root/JSA/power_experiment/run_imx8b.sh /root/JSA/power_experiment/demos_configurations/scale-1x/imx8b/cpu/ /root/JSA/power_experiment/measurements/scale-1x/imx8b/cpu/ imx8 10
 
-/root/JSA/power_experiment/run_imx8b.sh /root/JSA/power_experiment/demos_configurations/scale-3x/imx8b/all/ /root/JSA/power_experiment/measurements/scale-3x/imx8b/all/ imx8 10
-/root/JSA/power_experiment/run_imx8b.sh /root/JSA/power_experiment/demos_configurations/scale-3x/imx8b/cpu/ /root/JSA/power_experiment/measurements/scale-3x/imx8b/cpu/ imx8 10
+/root/JSA/power_experiment/run_imx8b.sh /root/JSA/power_experiment/demos_configurations/scale-3x/imx8b/all/ /root/JSA/power_experiment/measurements/scale-3x/imx8b/all/ imx8 30
+/root/JSA/power_experiment/run_imx8b.sh /root/JSA/power_experiment/demos_configurations/scale-3x/imx8b/cpu/ /root/JSA/power_experiment/measurements/scale-3x/imx8b/cpu/ imx8 30
 
 /root/JSA/power_experiment/run_imx8b.sh /root/JSA/power_experiment/demos_configurations/scale-1x/imx8b/all/ /root/JSA/power_experiment/measurements/scale-1x/imx8b/all-30/ imx8 30
 /root/JSA/power_experiment/run_imx8b.sh /root/JSA/power_experiment/demos_configurations/scale-1x/imx8b/cpu/ /root/JSA/power_experiment/measurements/scale-1x/imx8b/cpu-30/ imx8 30
