a360e55322b5a4407caec7d3e2abaa99e0da9565
Michal Sojka <michal.sojka@cvut.cz>
2022-04-14 13:29:47 +0200

    Add error checking to run scripts + simplify --column

--- experiments/run_imx8a.sh
+++ experiments/run_imx8a.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 run () {
     INPATH=$1
     OUTPATH=$2
@@ -16,7 +18,7 @@ run () {
 				echo "Output file already exists."
 		else	
 			cd /root/thermobench/build/benchmarks/autobench_2.0/single/
-			thermobench --verbose --fan-cmd=fan --fan-on=0 --time=$TIM --wait=50 --wait-timeout=60 --sensors_file=/root/thermobench/src/sensors.$PLAT --cpu-usage --column=CPU0_work_done --column=CPU1_work_done --column=CPU2_work_done --column=CPU3_work_done --column=CPU4_work_done --column=CPU5_work_done --output=$OUTPATH/$CURBENCH.csv -- demos-sched -p per_process -c $INPATH/$CURBENCH
+			thermobench --verbose --fan-cmd=fan --fan-on=0 --time=$TIM --wait=50 --wait-timeout=60 --sensors_file=/root/thermobench/src/sensors.$PLAT --cpu-usage --column=CPU{0,1,2,3,4,5}_work_done --output=$OUTPATH/$CURBENCH.csv -- demos-sched -p per_process -c $INPATH/$CURBENCH
 		fi
 	done	
 }
