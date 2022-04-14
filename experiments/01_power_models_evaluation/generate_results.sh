58ccabf462a15ebb908bc2070e78eb622a929947
benedond <benedond@fel.cvut.cz>
2022-04-14 08:57:05 +0200

    Add imx8b measurements with 30s/inst.

--- experiments/01_power_models_evaluation/generate_results.sh
+++ experiments/01_power_models_evaluation/generate_results.sh
@@ -22,3 +22,9 @@ python3 ../add_measured_data.py -r ./results/scale-3x-imx8a-cpu.csv -m ./measure
 python3 ../add_measured_data.py -r ./results/scale-3x-imx8b-all.csv -m ./measurements/scale-3x/imx8b/all/ -c power
 python3 ../add_measured_data.py -r ./results/scale-3x-imx8b-cpu.csv -m ./measurements/scale-3x/imx8b/cpu/ -c power
 
+
+# 30-s experiment at imx8-b
+python ../get_results.py instances/scale-1x/imx8b/all/ results/scale-1x-imx8b-all-30.csv
+python ../get_results.py instances/scale-1x/imx8b/cpu/ results/scale-1x-imx8b-cpu-30.csv
+python3 ../add_measured_data.py -r ./results/scale-1x-imx8b-all-30.csv -m ./measurements/scale-1x/imx8b/all-30/ -c power
+python3 ../add_measured_data.py -r ./results/scale-1x-imx8b-cpu-30.csv -m ./measurements/scale-1x/imx8b/cpu-30/ -c power
