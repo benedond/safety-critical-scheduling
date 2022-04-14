588efe417f3e8b7ae7289e6d83582438745de839
Michal Sojka <michal.sojka@cvut.cz>
2022-04-14 13:41:19 +0200

    Reindent, cleanup whitesapce

--- experiments/run_imx8.sh
+++ experiments/run_imx8.sh
@@ -6,22 +6,23 @@ run () {
     INPATH=$1
     OUTPATH=$2
     PLAT=$3
-	TIM=$4
+    TIM=$4
     FAN="${5:+--fan-cmd=fan --fan-on=0}" # if $5 has some value, replace it with --fan...
-    
+
     mkdir -p $OUTPATH
-	
-	for benchmark in $INPATH/*; do
-		CURBENCH=$(basename $benchmark)
-		echo "Processing $CURBENCH"
-	
-		if [ -f $OUTPATH/$CURBENCH.csv ]; then
-				echo "Output file already exists."
-		else	
-			cd /root/thermobench/build/benchmarks/autobench_2.0/single/
-			thermobench --verbose $FAN --time=$TIM --wait=50 --wait-timeout=60 --sensors_file=/root/thermobench/src/sensors.$PLAT --cpu-usage --column=CPU{0,1,2,3,4,5}_work_done --output=$OUTPATH/$CURBENCH.csv -- demos-sched -p per_process -c $INPATH/$CURBENCH
-		fi
-	done	
+
+    for benchmark in $INPATH/*; do
+        CURBENCH=$(basename $benchmark)
+
+        echo "Processing $CURBENCH"
+
+        if [ -f $OUTPATH/$CURBENCH.csv ]; then
+            echo "Output file already exists."
+        else
+            cd /root/thermobench/build/benchmarks/autobench_2.0/single/
+	    thermobench --verbose $FAN --time=$TIM --wait=50 --wait-timeout=60 --sensors_file=/root/thermobench/src/sensors.$PLAT --cpu-usage --column=CPU{0,1,2,3,4,5}_work_done --output=$OUTPATH/$CURBENCH.csv -- demos-sched -p per_process -c $INPATH/$CURBENCH
+        fi
+    done
 }
 # ===================================================================================================
 
