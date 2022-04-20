c7804543663276408daf60d5f70527bb2d4e37fd
benedond <benedond@fel.cvut.cz>
2022-04-20 14:11:31 +0200

    Add instance generator for coef. experiment.

--- experiments/04_benchmark_coefficients/generate_instances.py
+++ experiments/04_benchmark_coefficients/generate_instances.py
@@ -1,67 +1,59 @@
 #!/usr/bin/env python3
 import argparse
+import pandas as pd
 import sys
+import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import utils
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--window_length", "-w", default=1000, type=int, help="Length of the window in ms.")
-parser.add_argument("--env_file", "-e", type=str, help="Path to the environment file.")
-parser.add_argument("--bench_file", "-b", type=str, help="Path to the benchmarks file.")
-parser.add_argument("--out_folder", "-o", type=str, help="Path to the output folder where the instances will be created.")
+parser.add_argument("--env_file", "-e", type=str, required=True, help="Path to the environment file.")
+parser.add_argument("--bench_file", "-b", type=str, required=True, help="Path to the benchmarks file.")
+parser.add_argument("--out_folder", "-o", type=str, required=True, help="Path to the output folder where the instances will be created.")
 
 
-def generate_instance(args, benchmark, processor, cover_all):
-    instance = read_json(args.env_file)
+def generate_instance(args, benchmark, cmd, processor, cover_all):    
+    instance = utils.read_json(args.env_file)    
     instance["environment"]["majorFrameLength"] = args.window_length
-    
-    # TODO: write the generator
-    
-    workload = {}
-    while workload_is_none(workload):
-        workload = get_workload(instance["environment"], args, list(benchmarks["benchmark"].unique()))
-
+    length = args.window_length    
+    p_name = processor["name"]    
+                    
     ass_char = []
     tasks = []
     win_tasks = []
     
-    task_idx = 0
-    for p_idx, processor in enumerate(workload.keys()):
-        for core, w in zip(cores[processor], workload[processor]):
-            if w is None:
-                continue
-            else:                
-                benchmark, length = w
-                task_name = "{}_{}".format(task_idx, benchmark)
-                cmd, slope, intercept = get_cmd_slope_intercept(benchmark, processor, benchmarks_char)
+    core_ids = processor["coreIds"] if cover_all else [processor["coreIds"][0]]
+    for core_enum, c in enumerate(core_ids):
+        task_name = "{}_{}".format(c, benchmark)
+        slope, intercept = 0, 0
                 
-                cmd = 'TB_OPTS="--count=0 --work_done_every_sec=0.5 --work_done_str=CPU{}_work_done" '.format(cores_offset[processor][core]) + cmd
+        cmd_str = 'TB_OPTS="--count=0 --work_done_every_sec=0.5 --work_done_str=CPU{}_work_done" '.format(c) + cmd
                 
-                # Prepare assignment characteristics
-                ass_char.append({"command": cmd,
-                                 "task": task_name,
-                                 "resourceAssignments": [{"length": length,
-                                                          "slope": slope,
-                                                          "intercept": intercept,
-                                                          "processors": [{"processingUnits": 1, "processor": processor}]
-                                                          }]
-                                })
-                
-                # Prepare resource assignment
-                tasks.append({"assignmentIndex": 0,
-                               "command": cmd,
-                               "length": length,
-                               "name": task_name,
-                               "processors": [{"processor": processor, "processingUnits": 1}]
-                               })
-                
-                win_tasks.append({"length": length,
-                                  "processingUnit": core,
-                                  "processor": processor,
-                                  "start": 0,
-                                  "task": task_name})
-                
-                task_idx += 1
+        # Prepare assignment characteristics
+        ass_char.append({"command": cmd_str,
+                         "task": task_name,
+                         "resourceAssignments": [{"length": length,
+                                                  "slope": slope,
+                                                  "intercept": intercept,
+                                                  "processors": [{"processingUnits": 1, "processor": p_name}]
+                                                  }]
+                        })
+        
+        # Prepare resource assignment
+        tasks.append({"assignmentIndex": 0,
+                      "command": cmd_str,
+                      "length": length,
+                      "name": task_name,
+                      "processors": [{"processor": p_name, "processingUnits": 1}]
+                     })
+        
+        win_tasks.append({"length": length,
+                          "processingUnit": core_enum,
+                          "processor": p_name,
+                          "start": 0,
+                          "task": task_name})
                 
     instance["assignmentCharacteristics"] = ass_char
     instance["tasks"] = tasks
@@ -69,29 +61,31 @@ def generate_instance(args, benchmark, processor, cover_all):
                             "solutionTime": 0,
                             "solverMetadata": None,
                             "solverName": "RandomGenerator",
-                            "windows": [{"length": int(round(args.window_length * args.scale)), "tasks": win_tasks}]}
+                            "windows": [{"length": args.window_length, "tasks": win_tasks}]}
     
     return instance                
 
 if __name__ == "__main__":      
-    args = parser.parse_args()
-    random.seed(args.seed)
+    args = parser.parse_args()    
     
     if not os.path.exists(args.out_folder):
         os.makedirs(args.out_folder)
 
     benchmarks = utils.read_csv(args.bench_file)
     env = utils.read_json(args.env_file)
+     
+    for row in benchmarks.itertuples(index=False):
+        bench_name = row.benchmark
+        bench_cmd = row.command 
     
-    for benchmark in benchmarks["benchmark"].unique():
-        for p in env["processors"]:
-            p_name = p["name"]
-            
-            inst_1 = generate_instance(benchmark, p_name, False)
-            with open(os.path.join(args.out_folder, "{}_{}_one.json".format(benchmark, p_name)), "w") as f:
+        for p_idx, processor in enumerate(env["environment"]["processors"]):
+            p_name = processor["name"]
+                    
+            inst_1 = generate_instance(args, bench_name, bench_cmd, processor, False)
+            with open(os.path.join(args.out_folder, "{}_{}_one.json".format(bench_name, p_name)), "w") as f:
                 f.write(json.dumps(inst_1, indent=4, sort_keys=True))    
             
-            inst_all = generate_instance(benchmark, p_name, True)
-            with open(os.path.join(args.out_folder, "{}_{}_all.json".format(benchmark, p_name)), "w") as f:
+            inst_all = generate_instance(args, bench_name, bench_cmd, processor, True)
+            with open(os.path.join(args.out_folder, "{}_{}_all.json".format(bench_name, p_name)), "w") as f:
                 f.write(json.dumps(inst_all, indent=4, sort_keys=True))  
-        
+            
