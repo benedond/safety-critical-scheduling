9558c0d50f607c25979b59eb425dc083e3e86f8b
benedond <benedond@fel.cvut.cz>
2022-04-20 14:10:57 +0200

    Format.

--- experiments/01_power_models_evaluation/generate_instances.sh
+++ experiments/01_power_models_evaluation/generate_instances.sh
@@ -2,23 +2,16 @@
 
 # scale - 1x
 python generate_instances.py --window_length 1000 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8a-all.csv --out_folder instances/scale-1x/imx8a/all
-
 python generate_instances.py --window_length 1000 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8a-cpu.csv --out_folder instances/scale-1x/imx8a/cpu
-
 python generate_instances.py --window_length 1000 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8b-all.csv --out_folder instances/scale-1x/imx8b/all
-
 python generate_instances.py --window_length 1000 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8b-cpu.csv --out_folder instances/scale-1x/imx8b/cpu
 
 # scale - 3x
 python generate_instances.py --window_length 1000 --scale 3 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8a-all.csv --out_folder instances/scale-3x/imx8a/all
-
 python generate_instances.py --window_length 1000 --scale 3 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8a-cpu.csv --out_folder instances/scale-3x/imx8a/cpu
-
 python generate_instances.py --window_length 1000 --scale 3 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8b-all.csv --out_folder instances/scale-3x/imx8b/all
-
 python generate_instances.py --window_length 1000 --scale 3 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8b-cpu.csv --out_folder instances/scale-3x/imx8b/cpu
 
-# Scale 10x
+# scale - 10x
 python generate_instances.py --window_length 1000 --scale 10 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8b-all.csv --out_folder instances/scale-10x/imx8b/all
-
 python generate_instances.py --window_length 1000 --scale 10 --env_file ../../data/environment-imx8.json --char_file ../../data/characteristics-imx8b-cpu.csv --out_folder instances/scale-10x/imx8b/cpu
