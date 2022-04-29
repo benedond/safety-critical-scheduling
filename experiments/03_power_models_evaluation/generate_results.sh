f435308edb788c3b039bb718369f5409b80f69a7
benedond <benedond@fel.cvut.cz>
2022-04-29 14:19:12 +0200

    Add argparse arguments.

--- experiments/03_power_models_evaluation/generate_results.sh
+++ experiments/03_power_models_evaluation/generate_results.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Compute the results based on the pre-generated instances (solutions)
-python ../get_results.py instances/scale-1x/imx8a/all/ results/scale-1x-imx8a-all.csv
-python ../get_results.py instances/scale-1x/imx8a/cpu/ results/scale-1x-imx8a-cpu.csv
-python ../get_results.py instances/scale-1x/imx8b/all/ results/scale-1x-imx8b-all.csv
-python ../get_results.py instances/scale-1x/imx8b/cpu/ results/scale-1x-imx8b-cpu.csv
+python ../get_results.py -i instances/scale-1x/imx8a/all/ -o results/scale-1x-imx8a-all.csv -l ../../data/LR-coefficients.json -p imx8a
+python ../get_results.py -i instances/scale-1x/imx8a/cpu/ -o results/scale-1x-imx8a-cpu.csv -l ../../data/LR-coefficients.json -p imx8a
+python ../get_results.py -i instances/scale-1x/imx8b/all/ -o results/scale-1x-imx8b-all.csv -l ../../data/LR-coefficients.json -p imx8b
+python ../get_results.py -i instances/scale-1x/imx8b/cpu/ -o results/scale-1x-imx8b-cpu.csv -l ../../data/LR-coefficients.json -p imx8b
 
-python ../get_results.py instances/scale-3x/imx8a/all/ results/scale-3x-imx8a-all.csv
-python ../get_results.py instances/scale-3x/imx8a/cpu/ results/scale-3x-imx8a-cpu.csv
-python ../get_results.py instances/scale-3x/imx8b/all/ results/scale-3x-imx8b-all.csv
-python ../get_results.py instances/scale-3x/imx8b/cpu/ results/scale-3x-imx8b-cpu.csv
+python ../get_results.py -i instances/scale-3x/imx8a/all/ -o results/scale-3x-imx8a-all.csv -l ../../data/LR-coefficients.json -p imx8a
+python ../get_results.py -i instances/scale-3x/imx8a/cpu/ -o results/scale-3x-imx8a-cpu.csv -l ../../data/LR-coefficients.json -p imx8a
+python ../get_results.py -i instances/scale-3x/imx8b/all/ -o results/scale-3x-imx8b-all.csv -l ../../data/LR-coefficients.json -p imx8b
+python ../get_results.py -i instances/scale-3x/imx8b/cpu/ -o results/scale-3x-imx8b-cpu.csv -l ../../data/LR-coefficients.json -p imx8b
 
 # Add power measurements to the solutions
 python3 ../add_measured_data.py -r ./results/scale-1x-imx8a-all.csv -m ./measurements/scale-1x/imx8a/all/ -c power
@@ -24,7 +24,7 @@ python3 ../add_measured_data.py -r ./results/scale-3x-imx8b-cpu.csv -m ./measure
 
 
 # 30-s experiment at imx8-b
-python ../get_results.py instances/scale-1x/imx8b/all/ results/scale-1x-imx8b-all-30.csv
-python ../get_results.py instances/scale-1x/imx8b/cpu/ results/scale-1x-imx8b-cpu-30.csv
+python ../get_results.py -i instances/scale-1x/imx8b/all/ -o results/scale-1x-imx8b-all-30.csv -l ../../data/LR-coefficients.json -p imx8b
+python ../get_results.py -i instances/scale-1x/imx8b/cpu/ -o results/scale-1x-imx8b-cpu-30.csv -l ../../data/LR-coefficients.json -p imx8b 
 python3 ../add_measured_data.py -r ./results/scale-1x-imx8b-all-30.csv -m ./measurements/scale-1x/imx8b/all-30/ -c power
 python3 ../add_measured_data.py -r ./results/scale-1x-imx8b-cpu-30.csv -m ./measurements/scale-1x/imx8b/cpu-30/ -c power
