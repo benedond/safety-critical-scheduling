b202bde6f2bdc03b7513ab80dc204b08338b3f00
benedond <benedond@fel.cvut.cz>
2022-05-25 12:24:20 +0200

    Generate TX2 instances.

---
+++ experiments/04_solvers_thermal_evaluation/generate_demos_conf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# python ../generate_demos_config.py -i "imx8a/all" -f "solutions"
+# python ../generate_demos_config.py -i "imx8a/cpu" -f "solutions"
+# python ../generate_demos_config.py -i "imx8b/all" -f "solutions"
+# python ../generate_demos_config.py -i "imx8b/cpu" -f "solutions"
+python ../generate_demos_config.py -i "tx2/all" -f "solutions"
+python ../generate_demos_config.py -i "tx2/cpu" -f "solutions"
