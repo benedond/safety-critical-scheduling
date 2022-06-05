8febca6355e5ae9fef924d576f970941f0d2043e
Benedond <benedond@gmail.com>
2022-06-05 09:58:54 +0200

    Modify solving script to have correct params.

--- experiments/04_solvers_thermal_evaluation/solve_instances.sh
+++ experiments/04_solvers_thermal_evaluation/solve_instances.sh
@@ -34,13 +34,13 @@ solvers=( "HEUR" "ILP-IDLE-MIN" "ILP-IDLE-MAX" "ILP-SM-I" "ILP-SM-II" "QP-LR-UB"
 EXP_PATHs=( "HEUR" "ILP-IDLE" "ILP-IDLE" "ILP-SM-I" "ILP-SM-II" "QP-LR-UB")
 timelimit=300
 
-#cmds=( "" "" "--maximize" "" "" "--path_lr ../../data/LR-coefficients.json -p imx8a")
-# solve "imx8a/all"
-# solve "imx8a/cpu"
+cmds=( "" "" "--maximize" "" "" "--path_lr ../../data/LR-coefficients.json -p imx8a")
+solve "imx8a/all"
+solve "imx8a/cpu"
 
-#cmds=( "" "" "--maximize" "" "" "--path_lr ../../data/LR-coefficients.json -p imx8b")
-# solve "imx8b/all"
-# solve "imx8b/cpu"
+cmds=( "" "" "--maximize" "" "" "--path_lr ../../data/LR-coefficients.json -p imx8b")
+solve "imx8b/all"
+solve "imx8b/cpu"
 
 cmds=( "" "" "--maximize" "" "" "--path_lr ../../data/LR-coefficients.json -p tx2")
 solve "tx2/all"
