58531ed800cb103d7c18fe4fda8544a73328db92
benedond <benedond@fel.cvut.cz>
2022-06-30 15:13:25 +0200

    Extend visualizer colors to cover all benchmarks.

--- tools/src/visualizer_latex/main.py
+++ tools/src/visualizer_latex/main.py
@@ -12,16 +12,58 @@ DRAW_TASK_LEGEND = True
 EXPORT_ONLY_TIKZ = True
 MAJOR_FRAME_SYMBOL = "h" # symbol to describe the major frame length
 WINDOW_SYMBOL = "w"
-TASK_COLORS = {"dijkstra": "ClrFill1",
-               "fft": "ClrFill2",
-               "prime": "ClrFill3",
-               "sha": "ClrFill4",
-               "susan": "ClrFill5",
-               "test3": "ClrFill6",
-               "tinyrenderer": "ClrFill7",
-               "membench_rw_r_1M": "ClrFill8",
-               "membench_rw_r_4M": "ClrFill9",
-               }
+
+TASK_COLORS = {
+        "a2time-4K": "TaskClrFill0",
+        "a2time-4M": "TaskClrFill1",
+        "aifirf-4K": "TaskClrFill2",
+        "aifirf-4M": "TaskClrFill3",
+        "bitmnp-4K": "TaskClrFill4",
+        "bitmnp-4M": "TaskClrFill5",
+        "canrdr-4K": "TaskClrFill6",
+        "canrdr-4M": "TaskClrFill7",
+        "idctrn-4K": "TaskClrFill8",
+        "idctrn-4M": "TaskClrFill9",
+        "iirflt-4K": "TaskClrFill10",
+        "iirflt-4M": "TaskClrFill11",
+        "matrix-4K": "TaskClrFill12",
+        "matrix-4M": "TaskClrFill13",
+        "membench-1K-RO-R": "TaskClrFill14",
+        "membench-1K-RO-S": "TaskClrFill15",
+        "membench-1K-RW-R": "TaskClrFill16",
+        "membench-1K-RW-S": "TaskClrFill17",
+        "membench-1M-RO-R": "TaskClrFill18",
+        "membench-1M-RO-S": "TaskClrFill19",
+        "membench-1M-RW-R": "TaskClrFill20",
+        "membench-1M-RW-S": "TaskClrFill21",
+        "membench-4M-RO-R": "TaskClrFill22",
+        "membench-4M-RO-S": "TaskClrFill23",
+        "membench-4M-RW-R": "TaskClrFill24",
+        "membench-4M-RW-S": "TaskClrFill25",
+        "pntrch-4K": "TaskClrFill26",
+        "pntrch-4M": "TaskClrFill27",
+        "puwmod-4K": "TaskClrFill28",
+        "puwmod-4M": "TaskClrFill29",
+        "rspeed-4K": "TaskClrFill30",
+        "rspeed-4M": "TaskClrFill31",
+        "tblook-4K": "TaskClrFill32",
+        "tblook-4M": "TaskClrFill33",
+        "tinyrenderer-boggie": "TaskClrFill34",
+        "tinyrenderer-diablo": "TaskClrFill35",
+        "ttsprk-4K": "TaskClrFill36",
+        "ttsprk-4M": "TaskClrFill37"
+}
+
+#TASK_COLORS = {"dijkstra": "ClrFill1",
+#               "fft": "ClrFill2",
+#               "prime": "ClrFill3",
+#               "sha": "ClrFill4",
+#               "susan": "ClrFill5",
+#               "test3": "ClrFill6",
+#               "tinyrenderer": "ClrFill7",
+#               "membench_rw_r_1M": "ClrFill8",
+#               "membench_rw_r_4M": "ClrFill9",
+#               }
 
 def ms_to_cm(ms):
     """Convers milliseconds to centimeters"""
@@ -57,6 +99,44 @@ def run(inp, out):
         result.append(r"\definecolor{ClrFill8}{HTML}{e67f32}")
         result.append(r"\definecolor{ClrFill9}{HTML}{ce2123}")
         result.append(r"\definecolor{ClrFill10}{HTML}{531914}")
+        result.append(r"\definecolor{TaskClrFill0}{HTML}{696969}")
+        result.append(r"\definecolor{TaskClrFill1}{HTML}{556b2f}")
+        result.append(r"\definecolor{TaskClrFill2}{HTML}{a0522d}")
+        result.append(r"\definecolor{TaskClrFill3}{HTML}{7f0000}")
+        result.append(r"\definecolor{TaskClrFill4}{HTML}{191970}")
+        result.append(r"\definecolor{TaskClrFill5}{HTML}{808000}")
+        result.append(r"\definecolor{TaskClrFill6}{HTML}{008000}")
+        result.append(r"\definecolor{TaskClrFill7}{HTML}{3cb371}")
+        result.append(r"\definecolor{TaskClrFill8}{HTML}{008080}")
+        result.append(r"\definecolor{TaskClrFill9}{HTML}{4682b4}")
+        result.append(r"\definecolor{TaskClrFill10}{HTML}{9acd32}")
+        result.append(r"\definecolor{TaskClrFill11}{HTML}{00008b}")
+        result.append(r"\definecolor{TaskClrFill12}{HTML}{32cd32}")
+        result.append(r"\definecolor{TaskClrFill13}{HTML}{daa520}")
+        result.append(r"\definecolor{TaskClrFill14}{HTML}{7f007f}")
+        result.append(r"\definecolor{TaskClrFill15}{HTML}{b03060}")
+        result.append(r"\definecolor{TaskClrFill16}{HTML}{d2b48c}")
+        result.append(r"\definecolor{TaskClrFill17}{HTML}{ff0000}")
+        result.append(r"\definecolor{TaskClrFill18}{HTML}{00ced1}")
+        result.append(r"\definecolor{TaskClrFill19}{HTML}{ff8c00}")
+        result.append(r"\definecolor{TaskClrFill20}{HTML}{6a5acd}")
+        result.append(r"\definecolor{TaskClrFill21}{HTML}{0000cd}")
+        result.append(r"\definecolor{TaskClrFill22}{HTML}{7cfc00}")
+        result.append(r"\definecolor{TaskClrFill23}{HTML}{9400d3}")
+        result.append(r"\definecolor{TaskClrFill24}{HTML}{ba55d3}")
+        result.append(r"\definecolor{TaskClrFill25}{HTML}{00fa9a}")
+        result.append(r"\definecolor{TaskClrFill26}{HTML}{dc143c}")
+        result.append(r"\definecolor{TaskClrFill27}{HTML}{00bfff}")
+        result.append(r"\definecolor{TaskClrFill28}{HTML}{f08080}")
+        result.append(r"\definecolor{TaskClrFill29}{HTML}{ff7f50}")
+        result.append(r"\definecolor{TaskClrFill30}{HTML}{ff00ff}")
+        result.append(r"\definecolor{TaskClrFill31}{HTML}{1e90ff}")
+        result.append(r"\definecolor{TaskClrFill32}{HTML}{f0e68c}")
+        result.append(r"\definecolor{TaskClrFill33}{HTML}{ffff54}")
+        result.append(r"\definecolor{TaskClrFill34}{HTML}{dda0dd}")
+        result.append(r"\definecolor{TaskClrFill35}{HTML}{add8e6}")
+        result.append(r"\definecolor{TaskClrFill36}{HTML}{ff1493}")
+        result.append(r"\definecolor{TaskClrFill37}{HTML}{7fffd4}")
         result.append(r'\begin{document}')
     result.append('\\begin{{tikzpicture}}'.format(Y_SCALE))    
     result.extend(draw_image(data))  # Draw the figure in TikZ
